ec899a4848c6d4184ba5a952f464969a7520e830
Armando Caleno <armandoc8181@gmail.com>
2022-12-10 11:31:04 -0500

    cambio de nombre index

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -10,7 +10,7 @@ class PartnerController extends Controller
 {           
     public function index()
     {        
-        return view('partners.index');
+        return view('partners.index2');
     }
 
     public function create()
