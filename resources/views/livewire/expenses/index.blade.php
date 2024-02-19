408492a5d3a994f0671605f48157b8f93b12b370
Armando Caleño <armandoc8181@gmail.com>
2024-02-19 10:49:15 -0500

    Validación de edición y eliminación de facturas pagadas y abonadas

--- resources/views/livewire/expenses/index.blade.php
+++ resources/views/livewire/expenses/index.blade.php
@@ -1,25 +1,28 @@
 <div>
-    <div class="flex space-x-2 mb-4 ">  
-        {{-- Search --}}            
-        <x-jet-input type="text" class="flex-1" wire:model="search" placeholder="Buscar por número, proveedor o referencia..."/>             
-        
-        {{-- New input button --}}    
-        @can('transactions.expenses.create') 
-            <a href="{{ route('expenses.create') }}" class="px-4 py-2 bg-orange-500 rounded-md font-semibold text-white shadow-md hover:bg-opacity-80 focus:outline-none focus:ring active:text-gray-700 disabled:opacity-25 transition">Nuevo</a>               
-        @endcan 
+    <div class="flex space-x-2 mb-4 ">
+        {{-- Search --}}
+        <x-jet-input type="text" class="flex-1" wire:model="search"
+            placeholder="Buscar por número, proveedor o referencia..." />
+
+        {{-- New input button --}}
+        @can('transactions.expenses.create')
+            <a href="{{ route('expenses.create') }}"
+                class="px-4 py-2 bg-orange-500 rounded-md font-semibold text-white shadow-md hover:bg-opacity-80 focus:outline-none focus:ring active:text-gray-700 disabled:opacity-25 transition">Nuevo</a>
+        @endcan
     </div>
 
     {{-- Table --}}
     <div class="flex flex-col">
         <div class="-my-2 py-2 overflow-x-auto lg:-mx-8 lg:px-8">
-            <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">                                                                                      
+            <div
+                class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
                 <table class="min-w-full">
                     <thead>
-                        <tr>         
+                        <tr>
                             <th class="thead cursor-pointer" wire:click="order('number')">
                                 Numero
                                 {{-- sort --}}
-                                @if ($sort == 'number')                                    
+                                @if ($sort == 'number')
                                     @if ($direction == 'desc')
                                         <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
                                     @else
@@ -27,13 +30,13 @@
                                     @endif
                                 @else
                                     <i class="fas fa-sort float-right mt-1"></i>
-                                @endif 
+                                @endif
                             </th>
 
                             <th class="thead cursor-pointer" wire:click="order('date')">
                                 Fecha
                                 {{-- sort --}}
-                                @if ($sort == 'date')                                    
+                                @if ($sort == 'date')
                                     @if ($direction == 'desc')
                                         <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
                                     @else
@@ -41,52 +44,52 @@
                                     @endif
                                 @else
                                     <i class="fas fa-sort float-right mt-1"></i>
-                                @endif 
+                                @endif
                             </th>
 
                             <th class="thead">
-                                Proveedor                                
-                            </th>    
-                            
-                            <th class="thead">
-                                Referencia                                
-                            </th>
-                            
-                            <th class="thead">
-                                Pagos                                
-                            </th>
-                            
-                            <th class="thead">
-                                IVA                       
+                                Proveedor
                             </th>
 
                             <th class="thead">
-                                Total                       
+                                Referencia
                             </th>
 
                             <th class="thead">
-                                Saldo                       
+                                Pagos
                             </th>
 
                             <th class="thead">
-                                Estado                       
+                                IVA
                             </th>
-                            
+
+                            <th class="thead">
+                                Total
+                            </th>
+
+                            <th class="thead">
+                                Saldo
+                            </th>
+
+                            <th class="thead">
+                                Estado
+                            </th>
+
                             <th class="thead"></th>
                         </tr>
                     </thead>
 
                     <tbody class="bg-white">
-                        @if ($expenses->count())  
+                        @if ($expenses->count())
                             @foreach ($expenses as $item)
                                 <tr>
                                     <td class="row whitespace-nowrap">
                                         {{-- {{ $item->number }} --}}
                                         @php
-                                            echo substr($item->number, 0,3)."-".substr($item->number, 3,3)."-".substr($item->number,6,15);
+                                            echo substr($item->number, 0, 3) . '-' . substr($item->number, 3, 3) . '-' . substr($item->number, 6, 15);
                                         @endphp
-                                    </td>                                                                                                                              
-                                    
+                                    </td>
+
                                     <td class="row whitespace-nowrap">
                                         {{ $item->date }}
                                     </td>
@@ -123,36 +126,41 @@
                                     <td class="row text-center w-full">
                                         @if ($item->totalValueChecks() == $item->total)
                                             <div class="w-full bg-green-500 text-white p-1 rounded-md text-xs">
-                                                <span >Pagada</span>
+                                                <span>Pagada</span>
                                             </div>
                                         @else
                                             @if ($item->totalValueChecks($item->id) == 0)
                                                 <div class="w-full bg-red-500 text-white p-1 rounded-md text-xs">
-                                                    <span >Pendiente</span>
+                                                    <span>Pendiente</span>
                                                 </div>
                                             @else
                                                 <div class="w-full bg-blue-500 text-white p-1 rounded-md text-xs">
-                                                    <span >Abonada</span>
+                                                    <span>Abonada</span>
                                                 </div>
                                             @endif
-                                            
                                         @endif
-                                        
+
                                     </td>
 
                                     <td class="row font-medium">
-                                        <div class="flex space-x-4">                                                                                        
-                                            @can('transactions.expenses.edit') 
-                                                <a href="{{ route('expenses.edit', $item) }}" class="text-gray-600 hover:opacity-50"><i class="fa-solid fa-pen-to-square"></i></a>
-                                            @endcan   
-                                            
-                                            @can('transactions.expenses.delete') 
-                                                <a href="#" wire:click="delete({{ $item }})" class="text-orange-500 hover:opacity-50"><i class="fa-solid fa-trash-can"></i></a>
-                                            @endcan
+                                        <div class="flex space-x-4">
+                                            @if ($item->totalValueChecks($item->id) == 0)
+                                                @can('transactions.expenses.edit')
+                                                    <a href="{{ route('expenses.edit', $item) }}"
+                                                        class="text-gray-600 hover:opacity-50"><i
+                                                            class="fa-solid fa-pen-to-square"></i></a>
+                                                @endcan
+
+                                                @can('transactions.expenses.delete')
+                                                    <a href="#" wire:click="delete({{ $item }})"
+                                                        class="text-orange-500 hover:opacity-50"><i
+                                                            class="fa-solid fa-trash-can"></i></a>
+                                                @endcan
+                                            @endif
                                         </div>
                                     </td>
                                 </tr>
-                            @endforeach  
+                            @endforeach
                         @else
                             <tr>
                                 <td colspan="6">
@@ -160,36 +168,36 @@
                                         <strong>No se encontraron registros</strong>
                                     </div>
                                 </td>
-                            </tr>                            
-                        @endif                                                  
+                            </tr>
+                        @endif
                     </tbody>
-                </table>                
+                </table>
             </div>
-        </div>                            
+        </div>
     </div>
 
     {{-- Pagination --}}
     @if ($expenses->hasPages())
         <div class="px-4 py-4 bg-white border border-gray-200 mt-2 rounded-md shadow-lg">
-            {{ $expenses->links() }}   
+            {{ $expenses->links() }}
         </div>
-    @endif   
+    @endif
 
     {{-- Delete conformation modal --}}
     <x-jet-confirmation-modal wire:model="confirmingDeletion">
         <x-slot name="title">
             Eliminar compra
         </x-slot>
-    
+
         <x-slot name="content">
             ¿Estás seguro de eliminar esta compra?.
         </x-slot>
-    
+
         <x-slot name="footer">
             <x-jet-secondary-button wire:click="$toggle('confirmingDeletion')" wire:loading.attr="disabled">
-                Cancelar    
+                Cancelar
             </x-jet-secondary-button>
-    
+
             <x-jet-danger-button class="ml-2" wire:click="destroy" wire:loading.attr="disabled">
                 Eliminar
             </x-jet-danger-button>
